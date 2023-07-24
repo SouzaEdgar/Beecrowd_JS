5eb65616cce004b8e9c18b5a9fff0645f21e7873
SouzaEdgar <edgarss2013@gmail.com>
2023-07-24 15:49:09 -0300

    resolvido

--- 1161 - Soma de Fatoriais/script.js
+++ 1161 - Soma de Fatoriais/script.js
@@ -1,5 +1,5 @@
 var input = require('fs').readFileSync('./1161 - Soma de Fatoriais/stdin', 'utf8');
-var lines = parseInt(input.split('\n'));
+var lines = parseInt(input.split('\n')); // Não irei alterar essa variavel pois se trata de uma var padrão do BeeCrowd
 
 /*
 Leia dois valores inteiros M e N indefinidamente. A cada leitura, calcule e escreva a soma dos fatoriais de cada um dos valores lidos. Utilize uma variável apropriada, pois cálculo pode resultar em um valor com mais de 15 dígitos.
@@ -13,14 +13,23 @@ Para cada caso de teste de entrada, seu programa deve imprimir uma única linha,
 
 function fatorial(num) {
     let resultado = 1;
-    for (let x = 1; x < num+1; x++) {
+    for (let x = 1; x <= num; x++) {
         resultado *= x;
     }
     return resultado;
 }
-let num1 = parseInt(input.split(" ")[0]);
-let num2 = parseInt(input.split(" ")[1]);
 
-if (num1 <= 20 && num1 >= 0 && num2 <= 20 && num2 >= 0) {
-    console.log(fatorial(num1) + fatorial(num2));
+const nums = input.split("\n");
+for (let x in nums) {
+    const valor = nums[x].split(" ");
+    let num1 = parseInt(valor[0]);
+    let num2 = parseInt(valor[1]);
+
+    if (num1 <= 20 && num1 >= 0 && num2 <= 20 && num2 >= 0) {
+        if (Number.isSafeInteger(fatorial(num1) || Number.isSafeInteger(fatorial(num2)))) {
+            console.log((BigInt(fatorial(num1)) + BigInt(fatorial(num2))).toString());
+        } else {
+            console.log(fatorial(num1) + fatorial(num2));
+        }
+    }
 }
