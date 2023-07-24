6f7be5770738201d6154d54a79a6e696f2114343
SouzaEdgar <edgarss2013@gmail.com>
2023-07-24 14:21:35 -0300

    logica

--- 1161 - Soma de Fatoriais/script.js
+++ 1161 - Soma de Fatoriais/script.js
@@ -11,3 +11,16 @@ Saída
 Para cada caso de teste de entrada, seu programa deve imprimir uma única linha, contendo um número que é a soma de ambos os fatoriais (de M e N).
 */
 
+function fatorial(num) {
+    let resultado = 1;
+    for (let x = 1; x < num+1; x++) {
+        resultado *= x;
+    }
+    return resultado;
+}
+let num1 = parseInt(input.split(" ")[0]);
+let num2 = parseInt(input.split(" ")[1]);
+
+if (num1 <= 20 && num1 >= 0 && num2 <= 20 && num2 >= 0) {
+    console.log(fatorial(num1) + fatorial(num2));
+}
