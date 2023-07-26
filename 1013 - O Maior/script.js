cfb489fcd1333278069c707f6538d9d439216e40
SouzaEdgar <edgarss2013@gmail.com>
2023-07-25 21:56:35 -0300

    resolvido

--- 1013 - O Maior/script.js
+++ 1013 - O Maior/script.js
@@ -15,3 +15,14 @@ Saída
 Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
 */
 
+function maior(num1, num2) {
+    return ((num1 + num2 + Math.abs(num1 - num2))/2);
+}
+
+const nums = input.split(" ");
+const a = parseInt(nums[0]);
+const b = parseInt(nums[1]);
+const c = parseInt(nums[2]);
+const mAB = maior(a, b);
+
+console.log(`${maior(mAB, c)} eh o maior`);
