d494d14255f831a1561f81491058072aa35b677a
SouzaEdgar <edgarss2013@gmail.com>
2023-07-25 21:46:53 -0300

    novo problema

---
+++ 1013 - O Maior/script.js
@@ -0,0 +1,17 @@
+var input = require('fs').readFileSync('./1013 - O Maior/stdin', 'utf8');
+var lines = input.split('\n');
+
+/*
+Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
+
+
+
+Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
+
+Entrada
+O arquivo de entrada contém três valores inteiros.
+
+Saída
+Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+*/
+
