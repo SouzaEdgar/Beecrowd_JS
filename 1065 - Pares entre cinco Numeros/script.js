c3a7e8e73ab01889b40970753df3ef0ea6990522
SouzaEdgar <edgarss2013@gmail.com>
2023-08-08 21:43:44 -0300

    novo problema

---
+++ 1065 - Pares entre cinco Numeros/script.js
@@ -0,0 +1,13 @@
+var input = require('fs').readFileSync('./1060 - Pares entre cinco Numeros/stdin', 'utf8');
+var lines = input.split('\n');
+
+/*
+Faça um programa que leia 5 valores inteiros. Conte quantos destes valores digitados são pares e mostre esta informação.
+
+Entrada
+O arquivo de entrada contém 5 valores inteiros quaisquer.
+
+Saída
+Imprima a mensagem conforme o exemplo fornecido, indicando a quantidade de valores pares lidos.
+*/
+
