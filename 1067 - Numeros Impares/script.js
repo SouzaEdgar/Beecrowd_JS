90b3a088db06e820c424bb86c79642b5c980b070
SouzaEdgar <edgarss2013@gmail.com>
2023-08-11 19:45:14 -0300

    novo problema

--- 1067 - Numeros Impares/script.js
+++ 1067 - Numeros Impares/script.js
@@ -1,4 +1,4 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var input = require('fs').readFileSync('./1067 - Numeros Impares/stdin', 'utf8');
 var lines = input.split('\n');
 
 /*
---
+++ 1070 - Seis Numeros Impares/script.js
@@ -0,0 +1,14 @@
+var input = require('fs').readFileSync('./1070 - Seis Numeros Impares/stdin', 'utf8');
+var lines = input.split('\n');
+
+/*
+Leia um valor inteiro X. Em seguida apresente os 6 valores ímpares consecutivos a partir de X, um valor por linha, inclusive o X ser for o caso.
+
+Entrada
+A entrada será um valor inteiro positivo.
+
+Saída
+A saída será uma sequência de seis números ímpares.
+*/
+
+
